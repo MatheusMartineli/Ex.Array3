a70340188220dbe9f52370b269d112a665d76521
matheus.mmarcondes <matheus.mmarcondes@SJC0562930W10-1>
2024-11-22 10:18:20 -0300

    Adicionar arquivos de projeto.

---
+++ Ex.Array3/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Ex.Array3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lsbA = new System.Windows.Forms.ListBox();
+            this.lsbB = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(126, 362);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 42);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(617, 362);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 42);
+            this.btnSair.TabIndex = 1;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lsbA
+            // 
+            this.lsbA.FormattingEnabled = true;
+            this.lsbA.Location = new System.Drawing.Point(221, 51);
+            this.lsbA.Name = "lsbA";
+            this.lsbA.Size = new System.Drawing.Size(120, 251);
+            this.lsbA.TabIndex = 2;
+            // 
+            // lsbB
+            // 
+            this.lsbB.FormattingEnabled = true;
+            this.lsbB.Location = new System.Drawing.Point(446, 51);
+            this.lsbB.Name = "lsbB";
+            this.lsbB.Size = new System.Drawing.Size(120, 251);
+            this.lsbB.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsbB);
+            this.Controls.Add(this.lsbA);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.ListBox lsbA;
+        private System.Windows.Forms.ListBox lsbB;
+    }
+}
+
---
+++ Ex.Array3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex.Array3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lsbA.Items.Clear();
+            lsbB.Items.Clear();
+
+           
+            int[] vetorA = new int[15];
+            int[] vetorB = new int[15];
+            Random random = new Random();
+
+            
+            for (int i = 0; i < vetorA.Length; i++)
+            
+            {
+                vetorA[i] = random.Next(1, 101); 
+
+                
+                lsbA.Items.Add("Valor " + vetorA[i]);
+
+                
+                vetorB[i] = vetorA[i] * vetorA[i];
+
+                
+                lsbB.Items.Add("Valor " + vetorB[i]);
+            }
+        }
+    }
+
+}
